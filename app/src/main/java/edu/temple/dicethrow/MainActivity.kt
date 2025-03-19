d47eeaff786d2a685d1a4d7b0a617c153ba0f6c3
jslootsky <joshuaslootsky@gmail.com>
2025-03-19 15:35:54 -0400

    created a separate fragment container and gave it the same functionality as the first

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -2,12 +2,7 @@ package edu.temple.dicethrow
 
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.get
-import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,14 +12,16 @@ class MainActivity : AppCompatActivity() {
         //can potentially cause an error since this is executed asynchronously
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .add(R.id.fragmentContainerView, DieFragment.newInstance(20)) // Using newInstance()
+                .add(R.id.fragmentContainerView1, DieFragment.newInstance(20)) // Using newInstance()
+                .add(R.id.fragmentContainerView2, DieFragment.newInstance(20))
                 .commit()
         }
 
         //since the only functionality is to click a button, not really an issue
         findViewById<Button>(R.id.rollDiceButton).setOnClickListener{
-            (supportFragmentManager.
-            findFragmentById(R.id.fragmentContainerView) as DieFragment)
+            (supportFragmentManager.findFragmentById(R.id.fragmentContainerView1) as DieFragment)
+                .throwDie()
+            (supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as DieFragment)
                 .throwDie()
         }
     }
