1e218da794407fee7f3dc3afd3d8d2d7349da849
jslootsky <joshuaslootsky@gmail.com>
2025-03-20 11:17:04 -0400

    created new branch

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -12,13 +12,16 @@ class MainActivity : AppCompatActivity() {
         //can potentially cause an error since this is executed asynchronously
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .add(R.id.fragmentContainerView1, DieFragment.newInstance(20)) // Using newInstance()
+                .add(
+                    R.id.fragmentContainerView1,
+                    DieFragment.newInstance(20)
+                ) // Using newInstance()
                 .add(R.id.fragmentContainerView2, DieFragment.newInstance(20))
                 .commit()
         }
 
         //since the only functionality is to click a button, not really an issue
-        findViewById<Button>(R.id.rollDiceButton).setOnClickListener{
+        findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
             (supportFragmentManager.findFragmentById(R.id.fragmentContainerView1) as DieFragment)
                 .throwDie()
             (supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as DieFragment)
